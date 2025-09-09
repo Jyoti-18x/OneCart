05448e04d937eb1a67f59d3a6b6ba7906dd757fa
Jyoti Ranjan Bastia <jyotiranjanbastiaj@gmail.com>
2025-09-09 20:23:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://onecart-frontend-dom2.onrender.com" , "http://localhost:5174"],
+ origin:["https://onecart-frontend-dom2.onrender.com" , "https://onecart-admin-0hfm.onrender.com"],
  credentials:true
 }))
 
